c11adc1baa21082e109b14fc0699a4198d73db25
Jacek Jackowiak <jackowiakjacek2@gmail.com>
2022-06-13 08:53:56 +0200

    add all titleClickHandler

--- js/script.js
+++ js/script.js
@@ -1,33 +1,44 @@
+'use strict';
+
 /*document.getElementById('test-button').addEventListener('click', function() {
     const links = document.querySelectorAll('.titles a');
     console.log('links:', links);
-}); */
+}); 
+ */
 const titleClickHandler = function(event) {
+    event.preventDefault();
+    const clickedElement = this;
     console.log('Link was clicked!');
-    console.log(event)
+    console.log(event);
 
-    /* remove class 'active' from all article links  */
+    /* [DONE]remove class 'active' from all article links  */
     const activeLinks = document.querySelectorAll('.titles a.active');
 
     for (let activeLink of activeLinks) {
         activeLink.classList.remove('active');
     }
-    console.log("active")
+    console.log(activeLinks)
 
-    /* add class 'active' to the clicked link */
+    /* [IN PROGRESS] add class 'active' to the clicked link */
+    clickedElement.classList.add('active');
+    console.log('clickedElement:', clickedElement);
 
-    /* remove class 'active' from all articles */
-    const activeArticles = document.querySelectorAll('.posts a.active');
-
+    /*[DONE] remove class 'active' from all articles */
+    const activeArticles = document.querySelectorAll('.post.active');
     for (let activeArticle of activeArticles) {
         activeArticle.classList.remove('active');
     }
-
     /* get 'href' attribute from the clicked link */
+    const articleSelector = document.getElementById("href")
+    const href = clickedElement.getAttribute('href');
+    console.log(href);
 
     /* find the correct article using the selector (value of 'href' attribute) */
+    const targetArticle = document.querySelector(href);
+    console.log(targetArticle);
 
     /* add class 'active' to the correct article */
+    targetArticle.classList.add('active');
 }
 
 const links = document.querySelectorAll('.titles a');
